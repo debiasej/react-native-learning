0dfecf5956ec5d49d3ab0cbc43f42758262ee798
Mario de Biase <debiasej@gmail.com>
2017-05-01 19:09:58 +0200

    Make a HTTP request before the PhotoList renders

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class PhotoList extends Component {
+  componentWillMount() {
+    axios.get('https://jsonplaceholder.typicode.com/photos')
+      .then(respose => console.log(respose));
+  }
 
   render() {
     return (
